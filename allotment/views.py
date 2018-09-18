014b528439bb9ebd5ba1d80bf15577b4418d08c8
Ashiq SUltan <ashiqdrive@gmail.com>
2018-09-18 12:34:25 +0530

    Urls Included, Index page created

--- allotment/views.py
+++ allotment/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Shift, Department, Staff
+
+def index(request):
+	"""View Function of Home Page"""
+	StaffListCount=Staff.objects.count()
+	StaffNamesList=Staff.objects.filter(department__shift=1).order_by('name')
+
+	context={
+	'StaffListCount':StaffListCount,
+	'StaffNamesList':StaffNamesList
+	}
+
+	return render(request,'index.html', context=context)
